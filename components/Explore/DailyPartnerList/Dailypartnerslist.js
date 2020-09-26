0661acf3e9b9e52de14d1b947177add71869a2f8
Yash Pratap <yashp241195@gmail.com>
2020-09-26 11:00:52 +0530

    list item added

--- components/Explore/DailyPartnerList/Dailypartnerslist.js
+++ components/Explore/DailyPartnerList/Dailypartnerslist.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import SelectPartnerCard from '../SelectPartnerCard/SelectPartnerCard'
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(0),
+      color: theme.palette.text.secondary,
+    },
+  }));
 
 const Dailypartnerslist = () => {
-    
+    const classes = useStyles();
+
     const li = {
         dailySuggestions:[
             {
@@ -21,6 +36,29 @@ const Dailypartnerslist = () => {
 
     return (
         <div>
+            <br/>
+            <Grid container spacing={3}>
+                <Grid item xs={3}>
+                    <Paper className={classes.paper}>
+                        <SelectPartnerCard />
+                    </Paper>
+                </Grid>
+                <Grid item xs={3}>
+                    <Paper className={classes.paper}>
+                        <SelectPartnerCard />
+                    </Paper>
+                </Grid>
+                <Grid item xs={3}>
+                    <Paper className={classes.paper}>
+                        <SelectPartnerCard />
+                    </Paper>
+                </Grid>
+                <Grid item xs={3}>
+                    <Paper className={classes.paper}>
+                        <SelectPartnerCard />
+                    </Paper>
+                </Grid>
+            </Grid>
             <br/>
             <pre>
                 { JSON.stringify(li,null,2) }
